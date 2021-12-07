d2f6da6c7c643bb4bdb8a99a9f4f29c9e4482b92
Halla-S <86550752+Halla-S@users.noreply.github.com>
2021-12-07 15:27:21 +0000

    Fixing errors

--- src/App.js
+++ src/App.js
@@ -5,24 +5,25 @@ let babyNames = babyNamesData;
 babyNames = babyNames.sort((item1, item2) =>
   item1.name.localeCompare(item2.name));
 const BabyNames = (props) => {
-  let [searchTerm, setSearchTerm] = useState("");
-  let [list, setList] = useState(props.data);
-  let [addlist, setAddList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [list, setList] = useState(props.data);
+  const [addlist, setAddList] = useState([]);
   function handleRemove(clickedItemId) {
     const newList = list.filter((item) => item.id !== clickedItemId);
     const mylist = list.filter((item) => item.id === clickedItemId);
     setList(newList);
-    addlist = addlist.concat(mylist);
-    setAddList(addlist);
+    setAddList(addlist.concat(mylist));
+    // setAddList(addlist);
   }
 
   function handleRemoveFav(clickedItemId) {
     const removedFromFav = addlist.filter((item) => item.id !== clickedItemId);
     const addedToOriginal = addlist.filter((item) => item.id === clickedItemId);
-    list = list.concat(addedToOriginal);
-    list = list.sort((item1, item2) => item1.name.localeCompare(item2.name));
-    setList(list);
-
+    // setList(list.concat(addedToOriginal));
+    setList(
+      list
+        .concat(addedToOriginal)
+        .sort((item1, item2) => item1.name.localeCompare(item2.name)));
     setAddList(removedFromFav);
   }
   return (
